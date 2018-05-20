02bc1df2523e8b0dea36a40b3f4dc00d867a5e25
dmndc <dinomandic@hotmail.com>
2018-05-19 21:20:34 -0500

    feat: configure eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,32 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
 module.exports = {
-  entry: ['react-hot-loader/patch', './src/index.js'],
+  entry: ["react-hot-loader/patch", "./src/index.js"],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"]
       }
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ["*", ".js", ".jsx"]
   },
   output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: `${__dirname}/dist`,
+    publicPath: "/",
+    filename: "bundle.js"
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist",
     hot: true
   }
 };
